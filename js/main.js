7d1f041f4dc200e0d9bd25b5f77f692c2840efe7
pavhns20 <67502480+pavhns20@users.noreply.github.com>
2020-12-13 10:35:47 +1100

    updated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log("hi there");
+// console.log("hi there");
+// $(function() {
+//   while( $('.current').height() > $('.current').height() ) {
+//       $('#fitin div').css('font-size', (parseInt($('#fitin div').css('font-size')) - 1) + "px" );
+//   }
+// });
